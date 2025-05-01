c0bdb141b73a483fc6aca7016e1ceb81d6ef6fc8
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-01 14:56:26 -0600

    Implements wave 1 and creates a Task model, and adds functions to it to_dict and from_dict.

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,27 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+
+    def to_dict(self):
+        task_as_dict = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
+        return task_as_dict
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(
+            title=task_data["title"],
+            description=task_data["description"],
+            completed_at=task_data.get("completed_at")
+        )
+        return new_task
