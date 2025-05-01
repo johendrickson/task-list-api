f1321c447d75be4835c3ace508d62daa59c9a93c
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-01 15:57:35 -0600

    Implements wave 2. All tests for wave1 and wave2 passing. Creates a routes_utilities file. Creates CRUD routes and specifically the sort by asc/desc one required for wave 2.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .db import db, migrate
 from .models import task, goal
 import os
+from .routes.task_routes import bp as tasks_bp
 
 def create_app(config=None):
     app = Flask(__name__)
@@ -17,6 +18,6 @@ def create_app(config=None):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints here
+    app.register_blueprint(tasks_bp)
 
     return app
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,75 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request, Response
+from app.models.task import Task
+from .route_utilities import validate_model
+from ..db import db
+
+bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+
+    try:
+        new_task = Task.from_dict(request_body)
+        
+    except KeyError as error:
+        response = {"details": "Invalid data"}
+        abort (make_response(response, 400))
+        
+    db.session.add(new_task)
+    db.session.commit()
+    
+    return {"task": new_task.to_dict()}, 201
+
+@bp.get("")
+def get_all_tasks():
+    query = db.select(Task)
+
+    title_param = request.args.get("title")
+    if title_param:
+        query = query.where(Task.title.ilike(f"%{title_param}%"))
+
+    description_param = request.args.get("description")
+    if description_param:
+        query = query.where(Task.description.ilike(f"%{description_param}%"))
+
+    sort_param = request.args.get("sort")
+    if sort_param == "asc":
+        query = query.order_by(Task.title.asc())
+    elif sort_param == "desc":
+        query = query.order_by(Task.title.desc())
+    else:
+        query = query.order_by(Task.title)
+
+    tasks = db.session.scalars(query)
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    return tasks_response
+
+@bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_model(Task, task_id)
+
+    return {"task": task.to_dict()}
+
+@bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    task.completed_at = request_body.get("completed_at", None)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
