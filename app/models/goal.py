7cf8808d0ae8ef136df5c755f4f90d461dc646ae
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-06 14:32:02 -0600

    Adds goal_id to Task model. Creates a one-to-many relationship between Goal and Task. A goal has many tasks, a task belongs to one goal.

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,10 +1,11 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 
 
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
+    tasks: Mapped[list["Task"]] = relationship(back_populates="goal")
 
     def to_dict(self):
         goal_as_dict = {
@@ -15,7 +16,4 @@ class Goal(db.Model):
 
     @classmethod
     def from_dict(cls, goal_data):
-        new_goal = Goal(
-            title=goal_data["title"],
-        )
-        return new_goal
+        return Goal(title=goal_data["title"])
--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,16 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 from datetime import datetime
+from typing import Optional
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
     completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    
+    goal_id: Mapped[Optional[int]] = mapped_column(db.ForeignKey("goal.id"), nullable=True)
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
     def to_dict(self):
         task_as_dict = {
@@ -15,13 +19,19 @@ class Task(db.Model):
             "description": self.description,
             "is_complete": self.completed_at is not None
         }
+        
+        if self.goal_id is not None:
+            task_as_dict["goal_id"] = self.goal_id
+            
         return task_as_dict
+
     
     @classmethod
     def from_dict(cls, task_data):
         new_task = Task(
             title=task_data["title"],
             description=task_data["description"],
-            completed_at=task_data.get("completed_at")
+            completed_at=task_data.get("completed_at"),
+            goal_id=task_data.get("goal_id")
         )
         return new_task
