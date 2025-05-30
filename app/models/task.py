f4e0b8cc0c72b0ed577842282460fbd662e8b586
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-30 14:42:46 -0700

    Updates task model to fit best practices.

--- app/models/task.py
+++ app/models/task.py
@@ -8,7 +8,7 @@ class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
-    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
     goal_id: Mapped[Optional[int]] = mapped_column(db.ForeignKey("goal.id"), nullable=True)
     goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
@@ -19,13 +19,13 @@ class Task(db.Model):
             "description": self.description,
             "is_complete": self.completed_at is not None
         }
-        
+
         if self.goal_id is not None:
             task_as_dict["goal_id"] = self.goal_id
-        
+
         return task_as_dict
 
-    
+
     @classmethod
     def from_dict(cls, task_data):
         new_task = Task(
